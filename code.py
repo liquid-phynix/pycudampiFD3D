d83a9906d709665e6b643cb029ed1402ab4bbae3
Dexter Filmore <liquid.phynix@gmail.com>
2017-11-09 09:51:43 +0100

    checkpoint

--- code.py
+++ code.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python2
 
+def debug(msg):
+    print ('rank %d: ' + msg) % mpi.rank
+    
+def debug0(msg):
+    if master:
+        print msg
+
 import argparse
 parser = argparse.ArgumentParser(description = '3D Cuda finite difference solver')
 args = parser.parse_args()
 
 import numpy as np
 from mpi4py import MPI
-
-from parutils import pprint
 
 # globals
 mpi = MPI.COMM_WORLD
@@ -20,24 +25,21 @@ slave = not master
 rank_left = (mpi.rank - 1) % (mpi.size - 1)
 rank_right = (mpi.rank + 1) % (mpi.size - 1)
 
-pprint('/'*74)
-pprint(' Running on %d cores' % mpi.size)
-pprint('\\'*74)
+debug0('/'*74)
+debug0(' Running on %d cores' % mpi.size)
+debug0('\\'*74)
 
 mpi.Barrier()
 
 #lx, ly, lz = 10.0*2.0*np.pi, 1.0*2.0*np.pi, 10.0*2.0*np.pi
-lx, ly, lz = 20.0*2.0*np.pi, 2.0*2.0*np.pi, 20.0*2.0*np.pi
+lx, ly, lz = 100.0*2.0*np.pi, 1.0*2.0*np.pi, 25.0*2.0*np.pi
 bx, by, bz = 8, 8, 8
-gx, gy, gz = 20, 2, 20
+gx, gy, gz = 400, 4, 100
 nx, ny, nz = bx * gx, by * gy, bz * gz
 dimx, dimy, dimz = bx * gx + 6, by * gy + 6, bz * gz + 6
 dx, dy, dz = float(lx) / float(nx), float(ly) / float(ny), float(lz) / float(nz)
 ddm2 = (float(lx) / float(nx)) ** (-2.0)
 elements = dimx * dimy * dimz
-
-def debug(msg):
-    print ('rank %d: ' + msg) % mpi.rank
 
 # assigning GPU ids to ranks
 if master:
@@ -60,7 +62,7 @@ mpi.Barrier()
 # compiling cubin and loading it up
 if master:
     import subprocess
-    subprocess.check_call(['nvcc', '--cubin', '-arch', 'sm_20', 'kernel.cu'])
+    subprocess.check_call(['nvcc', '--cubin', '-arch', 'sm_20', 'kernel_semi.cu'])
     for i in range(mpi.size - 1):
         mpi.send(None, dest = i, tag = 0xdeadbee)
     debug('cubin compiled, msg sent')
@@ -71,7 +73,7 @@ else:
     device = cuda.Device(gpu_id)
     device.make_context()
     mpi.recv(source = master_rank, tag = 0xdeadbee)
-    mod = cuda.module_from_file('kernel.cubin')
+    mod = cuda.module_from_file('kernel_semi.cubin')
     debug('msg received, cubin loaded')
     debug('# of devices: %d' % cuda.Device.count())
     debug('gpu_id: %d' % gpu_id)
@@ -81,22 +83,36 @@ mpi.Barrier()
 
 def init_nucleus(out_ar):
     cos = np.cos
+    tanh = np.tanh
+    pi = np.pi
+    sq3 = np.sqrt(3.0)
+    def sc_rod(x, y, z):
+        return 0.166666666666666666667 * (cos(x) + cos(x + z) + cos(z) + cos(x) + cos(x - z) + cos(z))
     def bcc(x, y, z):
         return 0.166666666666666666667 * (cos(x + y) + cos(x + z) + cos(y + z) + cos(x - y) + cos(x - z) + cos(y - z))
+    def hex_rod(x, y, z):
+        return 0.166666666666666666667 * (cos(x) + cos(x/2.0 + sq3*z/2.0) + cos(-x/2.0 + sq3*z/2.0))
+    def hex_rod_rot(x, y, z):
+        return 0.166666666666666666667 * (cos(z) + cos(z/2.0 + sq3*x/2.0) + cos(-z/2.0 + sq3*x/2.0))
     # xvec = np.linspace(0, lx, nx, endpoint = False)
     # yvec = np.linspace(0, ly, ny, endpoint = False)
     # zvec = np.linspace(mpi.rank * lz, (mpi.rank + 1) * lz, nz, endpoint = False)
     # X, Y, Z = np.meshgrid(xvec, yvec, zvec)
-#    [X, Y, Z] = np.mgrid[0 : lx : dx, 0 : ly : dy, (mpi.rank * lz) : (mpi.rank + 1) * lz : dz]
-    [X, Y, Z] = np.mgrid[0 : lx : dx, 0 : ly : dy, 0 : lz : dz]
-    out_ar[3:-3, 3:-3, 3:-3] = 0.05 * bcc(X, Y, Z) - 0.55
+    [Z, Y, X] = np.mgrid[(mpi.rank * lz) : (mpi.rank + 1) * lz : (float(lz)/float(nz-0.5)), 0 : ly : dy, 0 : lx : dx]
+#    [Z, Y, X] = np.mgrid[0 : lz : dz, 0 : ly : dy, 0 : lx : dx]
+    #    out_ar[3:-3, 3:-3, 3:-3] = (1+tanh((8.0*pi)**2.0 - (X-lx/2.0)**2.0 - (Z-lz/2.0)**2.0))
+    out_ar[3:-3, 3:-3, 3:-3] = -0.255 \
+        + 0.5 * (1+tanh((8.0*pi)**2.0 - ((X-lx/2.0)**2.0 + (Z-lz/2.0)**2.0))) * (hex_rod(X, Y, Z) + (-0.25) - (-0.255)) \
+        + 0.5 * (1+tanh((8.0*pi)**2.0 - ((X-lx/2.0)**2.0 + (Z-3.0*lz/2.0)**2.0))) * (hex_rod_rot(X, Y, Z) + (-0.25) - (-0.255))
+
+    #    out_ar[3:-3, 3:-3, 3:-3] = -0.55 + (1+tanh((8.0*pi)**2.0 - (X-lx/2)**2.0 - (Z-lz/2)**2.0)) * 0.05 * bcc(X, Y, Z)
 
 # setting up arrays on GPUs
 if slave:
     h_domain_save = None
-    h_domain = -0.2 + (np.random.rand(dimx, dimy, dimz).astype(np.float32)-0.5) / 50.0
-    #    h_domain = np.zeros((dimx, dimy, dimz), dtype = np.float32)
-    #    init_nucleus(h_domain)
+    #    h_domain = -0.2 + (np.random.rand(dimx, dimy, dimz).astype(np.float32)-0.5) / 50.0
+    h_domain = np.zeros((dimz, dimy, dimx), dtype = np.float32)
+    init_nucleus(h_domain)
 
     d_domain = cuda.mem_alloc(h_domain.nbytes)
     d_domain_p = cuda.mem_alloc(h_domain.nbytes)
@@ -110,16 +126,16 @@ if slave:
     h_recv_right = np.zeros(dimx * dimy * 3, dtype = np.float32)
 
 else: # master
-    h_domains = [np.zeros((dimx - 6, dimz - 6), dtype = np.float32) for _ in range(mpi.size - 1)]
+    h_domains = [np.zeros((dimz - 6, dimx - 6), dtype = np.float32) for _ in range(mpi.size - 1)]
     #    h_domains = [np.zeros((dimx, dimz), dtype = np.float32) for _ in range(mpi.size - 1)]
     
 
 mpi.Barrier()
 
-pprint('ddm2 : %f' % ddm2)
+debug0('ddm2 : %f' % ddm2)
 
 iterations = 10000000
-savefreq = 1000
+savefreq = 100
 
 if slave:
     kernel_source = mod.get_function('kernel_source')
@@ -138,10 +154,10 @@ if slave:
     kernel_ghost_copy(d_domain_p, d_left, d_right, np.int32(dimx), np.int32(dimy), np.int32(dimz), block = (256, 1, 1), grid = (1, 1, 1))
     cuda.memcpy_dtoh(h_give_left, d_left)
     cuda.memcpy_dtoh(h_give_right, d_right)
-    r_left = mpi.Irecv(h_recv_left, source = rank_left, tag = ((rank_left << 8) | 0x51))
-    r_right = mpi.Irecv(h_recv_right, source = rank_right, tag = ((rank_right << 8) | 0x1e))
-    mpi.Isend(h_give_left, dest = rank_left, tag = ((mpi.rank << 8) | 0x1e))
-    mpi.Isend(h_give_right, dest = rank_right, tag = ((mpi.rank << 8) | 0x51))
+    r_left = mpi.Irecv(h_recv_left, source = rank_left, tag = ((rank_left << 16) | 0x51))
+    r_right = mpi.Irecv(h_recv_right, source = rank_right, tag = ((rank_right << 16) | 0x1e))
+    mpi.Isend(h_give_left, dest = rank_left, tag = ((mpi.rank << 16) | 0x1e))
+    mpi.Isend(h_give_right, dest = rank_right, tag = ((mpi.rank << 16) | 0x51))
     r_left.Wait()
     r_right.Wait()
     cuda.memcpy_htod(d_left, h_recv_left)
@@ -158,9 +174,10 @@ if slave:
         if iters % savefreq == 0:
             # copy domain to host
             cuda.memcpy_dtoh(h_domain, d_domain)
-            h_domain_save = h_domain[3:-3,3,3:-3].copy()
+#            h_domain_save[:] = h_domain[3:-3,3,3:-3].copy()[:]
+#            h_domain_save = h_domain[3:-3,3,3:-3].copy()
             #            h_domain_save = h_domain[:,3,:].copy()
-            mpi.Send(h_domain_save, dest = master_rank, tag = 0x5a43)
+            mpi.Send(h_domain[3:-3,3,3:-3].copy(), dest = master_rank, tag = 0x5a43)
             mpi.Barrier()
 
         iters += 1
@@ -183,10 +200,10 @@ if slave:
         cuda.memcpy_dtoh(h_give_left, d_left)
         cuda.memcpy_dtoh(h_give_right, d_right)
         # send & recv these
-        r_left = mpi.Irecv(h_recv_left, source = rank_left, tag = ((rank_left << 8) | 0x51))
-        r_right = mpi.Irecv(h_recv_right, source = rank_right, tag = ((rank_right << 8) | 0x1e))
-        mpi.Isend(h_give_left, dest = rank_left, tag = ((mpi.rank << 8) | 0x1e))
-        mpi.Isend(h_give_right, dest = rank_right, tag = ((mpi.rank << 8) | 0x51))
+        r_left = mpi.Irecv(h_recv_left, source = rank_left, tag = ((rank_left << 16) | 0x51))
+        r_right = mpi.Irecv(h_recv_right, source = rank_right, tag = ((rank_right << 16) | 0x1e))
+        mpi.Isend(h_give_left, dest = rank_left, tag = ((mpi.rank << 16) | 0x1e))
+        mpi.Isend(h_give_right, dest = rank_right, tag = ((mpi.rank << 16) | 0x51))
         r_left.Wait()
         r_right.Wait()
         # copy ghost z direction ghost zones from host to device
@@ -213,4 +230,4 @@ else: # master
 
 mpi.Barrier()
 
-pprint('finished %d iterations' % iterations)
+debug0('finished %d iterations' % iterations)
